532157eaf9631bbae1121443b7cbea918712199e
i5ting <i5ting@126.com>
2016-05-25 19:52:18 +0800

    init

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 global.log = console.log
 
-module.exports = function (name, opt) {
+module.exports = function (name, disable) {
   //
   global.debug = require('debug')(name ? name : 'log1');
   
-  if (process.env.NODE_ENV === 'production') {
-    log(process.env.NODE_ENV)
-    global.log = function(t){}
+  if (disable === true) {
+    _reset_log ()
   }
+  
+  if (process.env.NODE_ENV === 'production') {
+    _reset_log ()
+  }
+}
+
+function _reset_log () {
+  global.log = function(t){}
 }
